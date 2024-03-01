46aee01adcb48e5678c90b73b184f4a739e463e5
DevWeb13 <giuliano.loic@sfr.fr>
2024-03-01 17:45:15 +0100

    feat: Clone home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,47 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { Link, type DocumentHead } from "@builder.io/qwik-city";
+import { HiArrowRightOutline } from "@qwikest/icons/heroicons";
 
 export default component$(() => {
   return (
-    <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
-    </>
+    <main class="flex min-h-screen flex-col p-6">
+      <div class="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+        {/* <AcmeLogo /> */}
+      </div>
+      <div class="mt-4 flex grow flex-col gap-4 md:flex-row">
+        <div class="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+          <p class={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+            <strong>Welcome to Acme.</strong> This is the example for the{" "}
+            <Link
+              href="https://www.learn-qwik.com/learn/"
+              class="text-blue-500"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Qwik Learn Course
+            </Link>
+            , brought to you by{" "}
+            <Link
+              href="https://twitter.com/LaReponseDev"
+              class="text-blue-500"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @LaReponseDev
+            </Link>
+          </p>
+          <Link
+            href="/login"
+            class="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+          >
+            <span>Log in</span> <HiArrowRightOutline class="w-5 md:w-6" />
+          </Link>
+        </div>
+        <div class="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+          {/* Add Hero Images Here */}
+        </div>
+      </div>
+    </main>
   );
 });
 
